9fe5a9ad6807ad7a53d209436bbbf4ae3ddd1f62
unknown <tanmayjoshi072@gmail.com>
2023-10-16 12:08:43 -0700

    Animation Added

--- Landing_Page.js
+++ Landing_Page.js
@@ -1,5 +1,24 @@
 const SignUp=document.getElementById('SignUp_Section')
 const Login_Up=document.getElementById('Login_Section')
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.getElementById("Header-Section");
+    header.style.opacity = "1"; // Fade in slowly
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const infoPart = document.querySelector(".Info-Part");
+    infoPart.style.opacity='1'
+  });
+
+// Add this JavaScript code to your existing JavaScript file (Landing_Page.js)
+document.addEventListener("DOMContentLoaded", function () {
+    const signupsection = document.getElementById("SignUp_Section");
+    signupsection.style.left = "116px"; // Slide in from the left
+  });
+
+
+
+
 function SignUP(){
     
     SignUp.style.display='block'
