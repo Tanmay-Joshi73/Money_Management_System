4a22d51f083b35c3660e211f45c771250313a8d7
unknown <tanmayjoshi072@gmail.com>
2023-10-27 08:10:19 -0700

    Route Created

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const app = express();
 
 // Read HTML files
-const LandingPage = fs.readFileSync(`${__dirname}/index.html`, 'utf-8');
-const HomePage = fs.readFileSync(`${__dirname}/home.html`, 'utf-8');
+const HomePage=fs.readFileSync(`${__dirname}/index.html`,'utf-8')
 
 // Serve static assets
 app.use('/css', express.static('css'));
@@ -13,17 +12,9 @@ app.use('/Logo', express.static('Logo'));
 app.use('/SampleImage', express.static('SampleImage'));
 app.use('/Images',express.static('Images'))
 
-app.post('/Home', (req, res) => {
-    // No need to manually set headers; Express handles it.
-    res.send(HomePage);
-});
-
-app.get('/Land', (req, res) => {
-    // No need to manually set headers; Express handles it.
-    res.send(LandingPage);
-});
-
-
+app.get('/Home',(req,res)=>{
+    res.end(HomePage)
+})
 
 app.listen(8000, '127.0.0.1', () => {
     console.log('Server is running on http://127.0.0.1:8000');
