bf7aeeb8bf402f5ec6994bb187cf2ecc922280ce
unknown <tanmayjoshi072@gmail.com>
2023-11-21 22:35:22 +0530

    routing issue solved

--- Controller/Usercontroller.js
+++ Controller/Usercontroller.js
@@ -1,8 +1,8 @@
-// const express=require('express')
-// const fs=require('fs')
-// const HomePage=fs.readFileSync(`${__dirname}/../index.html`,'utf-8')
-// const LoginPage=fs.readFileSync(`${__dirname}/../Authenticate.html`,'utf-8')
-// const CreatePage1=fs.readFileSync(`${__dirname}/../Authenticate1.html`,'utf-8')
+const express=require('express')
+const fs=require('fs')
+const HomePage=fs.readFileSync(`${__dirname}/../index.html`,'utf-8')
+const LoginPage=fs.readFileSync(`${__dirname}/../Authenticate.html`,'utf-8')
+const CreatePage1=fs.readFileSync(`${__dirname}/../Authenticate1.html`,'utf-8')
 // const app=require('../server.js')
 // const path=require('path')
 // app.use('/css', express.static(path.join(__dirname, '../css')));
@@ -11,14 +11,14 @@
 // app.use('/SampleImage', express.static(path.join(__dirname, '../SampleImage')));
 // app.use('/Images', express.static(path.join(__dirname, '../Images')));
 
-// exports.Home=async(req,res)=>{
-//     await res.end(HomePage)
-// }
-// exports.LoginPage=async(req,res)=>{
-//     await res.end(LoginPage)
-// }
-// exports.CreatePage1=async(req,res)=>{
-//     await res.end(CreatePage1)
-// }
+exports.Home=async(req,res)=>{
+    await res.end(HomePage)
+}
+exports.LoginPage=async(req,res)=>{
+    await res.end(LoginPage)
+}
+exports.CreatePage1=async(req,res)=>{
+    await res.end(CreatePage1)
+}
 
 
--- Routes/route.js
+++ Routes/route.js
@@ -1,7 +1,7 @@
-// const express=require('express')
-// const router=express.Router()
-// const {Home,LoginPage,CreatePage1}=require('./../Controller/Usercontroller')
-// router.route('/Home').get(Home)
-// router.route('/Login').get(LoginPage)
-// router.route('/Page1').post(CreatePage1)
-// module.exports=router
+const express=require('express')
+const router=express.Router()
+const {Home,LoginPage,CreatePage1}=require('./../Controller/Usercontroller')
+router.route('/Home').get(Home)
+router.route('/Login').get(LoginPage)
+router.route('/Create').post(CreatePage1)
+module.exports=router
