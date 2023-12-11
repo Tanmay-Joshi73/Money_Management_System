f2f6f6da8dff00581e171644ca70b4dc092fae44
unknown <tanmayjoshi072@gmail.com>
2023-12-11 23:59:03 +0530

    New Route Is Create with the mailing system

--- Controller/Usercontroller.js
+++ Controller/Usercontroller.js
@@ -1,7 +1,10 @@
 const express=require('express')
 const nodemailer=require('nodemailer')
 const bodyparser=require('body-parser')
-
+let ClientEmail;
+let ClintName;
+let ClientPassword;
+let ClientUserName;
 const fs=require('fs')
 const HomePage=fs.readFileSync(`${__dirname}/../index.html`,'utf-8')
 const LoginPage=fs.readFileSync(`${__dirname}/../Authenticate.html`,'utf-8')
@@ -16,7 +19,68 @@ exports.LoginPage=async(req,res)=>{
 }
 exports.CreatePage1=async(req,res)=>{
     await res.end(CreatePage1)
+     ClientEmail=req.body.UserEmail
+     
+    
 }
+exports.ProfiePage = async (req, res) => {
+    try {
+     
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure: true,
+        auth: {
+          user: process.env.UserEmail,
+          pass: process.env.UserPass
+        }
+      });
+  
+    
+      const clientName = req.body.fullName;
+      const clientUserName = req.body.Username;
+      const clientPassword = req.body.password;
+      const recipientEmail = ClientEmail; // Replace with the actual recipient email
+  
+      // Your logic for processing the profile page data
+  
+      // Sending an email
+      const mailOptions = {
+        from: 'tanmayjoshi072@gmail.com',
+        to: recipientEmail,
+        subject: 'Thanks For Joining US ,Keep Helping Us Like This',
+        text: `
+          Dear Recipient,
+  
+          The profile page has been Create for the user:
+          - Name: ${clientName}
+          - Username: ${clientUserName}
+          - Password: ${clientPassword}
+  
+          Thank you.
+        `
+      };
+  
+      // Call the function to send email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            // console.log('Email sent: ' + info.response);
+            res.end('Welcome To The Home Page My Boi')
+        }
+    })
+    }catch(err){
+        console.log(err)
+    }}
+  
+
+ 
+  
+
+
+
+
 exports.sendMail=async(req,res)=>{
 const workEmail=await req.body.workemail
 const transportar=nodemailer.createTransport({
@@ -31,7 +95,7 @@ const transportar=nodemailer.createTransport({
 const MailOptions={
     from: 'tanmayjoshi072@gmail.com',
     to: workEmail,
-    subject: 'New Form Submission',
+    subject: 'Thanks For Joining , Keep Supporting Us',
     text: `
     Title:  Money Manager: Empowering Your Financial Journey
     
--- Routes/route.js
+++ Routes/route.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const router=express.Router()
-const {Home,LoginPage,CreatePage1,sendMail}=require('./../Controller/Usercontroller')
+const {Home,LoginPage,CreatePage1,sendMail,ProfiePage}=require('./../Controller/Usercontroller')
 router.route('/Home').get(Home)
 router.route('/Login').get(LoginPage)
-router.route('/Create').get(CreatePage1)
+router.route('/Create').post(CreatePage1)
 router.route('/submitform').post(sendMail)
+router.route('/Profile').post(ProfiePage)
 module.exports=router
