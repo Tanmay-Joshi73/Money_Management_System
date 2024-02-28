0b1ce0146cc198526d3c5a7a39774a06b643f76e
Tanmay-Joshi73 <tanmayjoshi072@gmail.com>
2024-02-28 23:57:02 +0530

    Data according to the date has been added

--- Controller/TrackerController.js
+++ Controller/TrackerController.js
@@ -1,27 +1,38 @@
-const express=require('express')
-const {username}=require('./Usercontroller.js')
-const model=require('./../UserDB/data.js')
-const bodyparser=require('body-parser')
-const TrackerTour=require('../UserDB/data.js')
-exports.Tracker=(req,res)=>{
+const express = require('express')
+const { username } = require('./Usercontroller.js')
+const model = require('./../UserDB/data.js')
+const bodyparser = require('body-parser')
+const TrackerTour = require('../UserDB/data.js')
+exports.Tracker = (req, res) => {
     res.render('tracker')
 }
-exports.SavedData=async(req,res)=>{
-        const Expense_Data=req.body
-        const username=req.session.username
-        console.log(username)
-        try{
-        const dataata=await TrackerTour.findOneAndUpdate({
-            UserName:username
-        },
-        {$push:{Data:Expense_Data}},
-        {new:true}
-        )
-         
-         
-    }
-    catch(err){
-        console.log(err)
+exports.SavedData = async (req, res) => {
+    const Data = new Date()
+    const formattedDate = Data.toISOString().split('T')[0]; // Extracting YYYY-MM-DD
+    const username = req.session.username
+    let Expense_Data = req.body
+    if (!Array.isArray(Expense_Data)) {
+        // If it's not an array, convert it to an array with a single element
+        Expense_Data = [Expense_Data];
     }
 
-}
+    const data = await TrackerTour.findOneAndUpdate(
+        { UserName: username },
+        { $addToSet: { [`Data.${formattedDate}`]: { $each: Expense_Data } } },
+        { 
+            new: true,
+            upsert:true,
+            projection: { Username: 1, Data: 1, _id: 0 } // Projection to include only Username and Data fields
+        }
+    );
+
+    // Handle the response
+    
+};
+
+
+
+
+
+
+
--- UserDB/data.js
+++ UserDB/data.js
@@ -1,9 +1,21 @@
 const mongoose=require('mongoose')
-const Expense_Track=mongoose.Schema({
+const Expense_Track1=mongoose.Schema({
     UserName:{type:String,ref:"Data"},
     Data:{
-        type:Array
+        type:Map,
+        of: [[{ type: mongoose.Schema.Types.Mixed }]]
     }
 })
-const Expense_Collection_Data=mongoose.model("Expense_Collection_Data",Expense_Track)
-module.exports=Expense_Collection_Data
+const Expense_Collection_Data1=mongoose.model("Expense_Collection_Data",Expense_Track1)
+module.exports=Expense_Collection_Data1
+
+
+
+/*
+  const dataata=await TrackerTour.findOneAndUpdate({
+            UserName:username
+        },
+        {$push:{Data:Expense_Data}},
+        {new:true}
+        )
+*/
